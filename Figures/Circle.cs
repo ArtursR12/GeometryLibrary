22cec1796e9d71507aedefed2803ab29be2e3897
Arturs Rizijs <artursrizijs12@gmail.com>
2024-03-09 20:04:00 +0200

    Initial commit

---
+++ Figures/Circle.cs
@@ -0,0 +1,23 @@
+﻿using GeometryLibrary.Interfaces;
+
+namespace GeometryLibrary.Figures
+{
+    public class Circle : IFigure
+    {
+        public double Radius { get; }
+
+        public Circle(double radius)
+        {
+            if (radius <= 0)
+            {
+                throw new ArgumentException("Radius must be greater than zero.", nameof(radius));
+            }
+            Radius = radius;
+        }
+
+        public double CalculateArea()
+        {
+            return Math.PI * Radius * Radius;
+        }
+    }
+}
---
+++ Figures/Triangle.cs
@@ -0,0 +1,45 @@
+﻿using GeometryLibrary.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeometryLibrary.Figures
+{
+    public class Triangle : IFigure
+    {
+        public double SideA { get; }
+        public double SideB { get; }
+        public double SideC { get; }
+
+        public Triangle(double sideA, double sideB, double sideC)
+        {
+            if (sideA <= 0 || sideB <= 0 || sideC <= 0)
+            {
+                throw new ArgumentException("Sides must be greater than zero.");
+            }
+
+            if (sideA + sideB <= sideC || sideA + sideC <= sideB || sideB + sideC <= sideA)
+            {
+                throw new ArgumentException("The sum of any two sides must be greater than the third side.");
+            }
+
+            SideA = sideA;
+            SideB = sideB;
+            SideC = sideC;
+        }
+
+        public double CalculateArea()
+        {
+            double s = (SideA + SideB + SideC) / 2;
+            return Math.Sqrt(s * (s - SideA) * (s - SideB) * (s - SideC));
+        }
+
+        public bool IsRightAngled()
+        {
+            var sides = new[] { SideA, SideB, SideC }.OrderBy(x => x).ToArray();
+            return Math.Abs(sides[2] * sides[2] - (sides[0] * sides[0] + sides[1] * sides[1])) < 1E-10;
+        }
+    }
+}
