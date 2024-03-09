eb61292579aadf9dd27ac9e33a2bb10de1be0204
Arturs Rizijs <artursrizijs12@gmail.com>
2024-03-09 20:11:26 +0200

    Add GeometryLibrary.Tests project

---
+++ GeometryLibrary.Tests/FiguresTests/CircleTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GeometryLibrary.Figures; 
+
+namespace GeometryLibrary.Tests.FiguresTests
+{
+    [TestClass]
+    public class CircleTests
+    {
+        [TestMethod]
+        public void CalculateArea_WithRadius5_ReturnsCorrectArea()
+        {
+            // Arrange
+            var circle = new Circle(5);
+            var expectedArea = Math.PI * 25; // Площадь круга = πr²
+
+            // Act
+            var area = circle.CalculateArea();
+
+            // Assert
+            Assert.AreEqual(expectedArea, area, 0.001, "Calculated area should be close to the expected value.");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Circle_NegativeRadius_ThrowsArgumentException()
+        {
+            var circle = new Circle(-5);
+        }
+    }
+}
---
+++ GeometryLibrary.Tests/FiguresTests/TriangleTests.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GeometryLibrary.Figures;
+
+namespace GeometryLibrary.Tests.FiguresTests
+{
+    [TestClass]
+    public class TriangleTests
+    {
+        [TestMethod]
+        public void CalculateArea_WithSides3And4And5_ReturnsCorrectArea()
+        {
+            // Arrange
+            var triangle = new Triangle(3, 4, 5);
+            var expectedArea = 6; 
+
+            // Act
+            var area = triangle.CalculateArea();
+
+            // Assert
+            Assert.AreEqual(expectedArea, area, 0.001, "Calculated area should be close to the expected value.");
+        }
+
+        [TestMethod]
+        public void IsRightAngled_WithSides3And4And5_ReturnsTrue()
+        {
+            // Arrange
+            var triangle = new Triangle(3, 4, 5);
+
+            // Act
+            var isRightAngled = triangle.IsRightAngled();
+
+            // Assert
+            Assert.IsTrue(isRightAngled, "Triangle with sides 3, 4, 5 should be right-angled.");
+        }
+
+        // Тест на невалидные стороны, которые не могут формировать треугольник
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Triangle_InvalidSides_ThrowsArgumentException()
+        {
+            var triangle = new Triangle(1, 2, 3);
+        }
+    }
+}
